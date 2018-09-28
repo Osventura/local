0fc651ea3f8d8912b2c7433cbe1179d30e82d043
osmar <aventura140.r@gmail.com>
2018-09-28 17:49:47 -0400

    boton estudiante

--- src/vista/inicio.java
+++ src/vista/inicio.java
@@ -36,7 +36,12 @@ public class inicio extends javax.swing.JFrame {
 
         jLabel1.setText("CONFERENCIA DE SIS2");
 
-        jButton1.setText("jButton1");
+        jButton1.setText("ESTUDIANTE");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("SECRETARIA");
 
@@ -58,7 +63,7 @@ public class inicio extends javax.swing.JFrame {
                         .addComponent(jButton2)
                         .addGap(120, 120, 120)
                         .addComponent(jButton3)))
-                .addContainerGap(133, Short.MAX_VALUE))
+                .addContainerGap(111, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -75,6 +80,10 @@ public class inicio extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
