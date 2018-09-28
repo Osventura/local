b687aa5019b4b57b5beac53bd38023a7f104d86c
roger <rog_am_33@hotmail.com>
2018-09-28 16:46:26 -0400

    roy

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -15,7 +15,12 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic her
+        
+        ///proyecto de sis2
+        
+        
+        
     }
     
 }
